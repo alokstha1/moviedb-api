f98397ae8cac48afe10853f3983c683c7193dcba
Alok Shrestha <alokstha1@gmail.com>
2019-12-03 17:45:59 +0545

    Showed the similar movies

--- detail.php
+++ detail.php
@@ -66,7 +66,51 @@ if ( isset( $_GET['movie_id'] ) && !empty( $_GET
     if ($simliar_movies_err) {
       echo "cURL Error #:" . $simliar_movies_err;
     } else {
-      echo $similar_movie_response;
+      // echo $similar_movie_response;
+        $similar_json_resources = json_decode($similar_movie_response);
+
+        if ( !empty( $similar_json_resources->results ) ) {
+
+            ?>
+            <ul>
+                <?php
+                    foreach ($similar_json_resources->results as $result_value) {
+
+                        $movie_link = 'http://localhost/test-movie-api/detail.php?movie_id='.$result_value->id;
+                        ?>
+                        <li>
+                            <a href="<?php echo $movie_link; ?>"><?php echo $result_value->title; ?></a>
+                        </li>
+                        <?php
+                    }
+                ?>
+            </ul>
+            <?php
+        }
     }
 
 }
+?>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript">
+     $(function(){
+        $('a.add-favorite').on('click', function(e){
+            e.preventDefault();
+            var _this       = $(this);
+            var movie_id    = _this.data('id');
+           $.ajax({
+
+              method: "POST",
+              url: "add-to-favorite.php",
+              data:{
+                movie_id: movie_id,
+                api_key:'f200ea93d28d03201a0e1caee1ebd3e6',
+              },
+            }).done(function(response) {
+                alert(response);
+              _this.removeClass( "add-favorite" );
+              _this.text( "Added" );
+            });
+        });
+    });
+</script>
